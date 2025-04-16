c936f2f14508384ee9fbe1836e811730851c982f
Carlos Humberto <camehu@gmail.com>
2025-04-16 11:20:41 -0300

    up Rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  {path:'api/v1',component:AppComponent },
+  {path:'/',component:AppComponent },
 ];
